3842b7a221a9c5c0e6a969c64b6c0a47ea8822ee
Nik Osvalds <nosvalds@gmail.com>
2020-09-07 17:20:53 +0100

    load and move file to a directory

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const _ = require('lodash');
+const xmlConvert = require('xml-js');
 
 const app = express();
 
@@ -20,6 +21,29 @@ app.use(morgan('dev'));
 
 //start app 
 const port = process.env.PORT || 3000;
+
+// POST /iatidoc
+app.post('/iatidoc', async (req, res) => {
+    try {
+        if(!req.files) {
+            res.status(400).send({
+                error: true,
+                message: 'No file uploaded'
+            });
+        } else {
+            // Get uploaded document
+            let xmlUpload = req.files.xmlUpload // .xmlUpload needs to be name of input field in HTML
+
+            xmlUpload.mv(__dirname + '/file_storage/' + xmlUpload.name);
+            
+            res.send({
+                message: "file saved"
+            })
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+})
 
 app.listen(port, () => 
   console.log(`App is listening on port ${port}.`)
